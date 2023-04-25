37f6d48f10d64de034455ea98de046cb6fd14a30
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-04-25 19:32:57 -0300

    requisito 5, mas atrapalhou o requisito 2(ver depois)

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,11 +2,21 @@ export const fetchProduct = async () => {
 
 };
 
-export const fetchProductsList = async (product = '') => {
-  if (product !== '') {
-    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
-    const data = await response.json();
-    return data.results;
+export const fetchProductsList = async (product = null) => {
+  // if (product !== null) {
+  //   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+  //   const data = await response.json();
+  //   return data.results;
+  // }
+  // throw new Error('Termo de busca não informado');
+  try {
+    if (product !== null) {
+      const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+      const data = await response.json();
+      return data.results;
+    }
+    throw new Error('Termo de busca não informado');
+  } catch (error) {
+    console.error(error);
   }
-  throw new Error('Termo de busca não informado');
 };
--- src/main.js
+++ src/main.js
@@ -7,10 +7,23 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 // add os produtos na tela
 const list = await fetchProductsList('computador');
-list.forEach(async (e) => {
-  const product = await createProductElement(e);
-  document.querySelector('.products').appendChild(product);
-});
+console.log(list);
+try {
+  if (list !== undefined && list.length !== 0) {
+    list.forEach(async (e) => {
+      const product = await createProductElement(e);
+      document.querySelector('.products').appendChild(product);
+    });
+  } else {
+    throw new Error('Algum erro ocorreu, recarregue a página e tente novamente');
+  }
+} catch (error) {
+  console.error(error);
+  const h2 = document.createElement('h2');
+  h2.innerHTML = error.message;
+  h2.className = 'error';
+  document.querySelector('.products').appendChild(h2);
+}
 // remove o 'caregando...' da tela
 const load = document.querySelector('.loading');
 load.parentNode.removeChild(load);
