da47c6cfd094c0f00c68500f58a7d5bddf59e0b0
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-04-25 16:53:44 -0300

    requisito 4

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+// add os produtos na tela
 const list = await fetchProductsList('computador');
 list.forEach(async (e) => {
   const product = await createProductElement(e);
   document.querySelector('.products').appendChild(product);
 });
+// remove o 'caregando...' da tela
+const load = document.querySelector('.loading');
+load.parentNode.removeChild(load);
